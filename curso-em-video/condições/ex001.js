4251b804cf3ad031e186f564603b5b57e42f2e15
MatheusBigui <153760019+MatheusBigui@users.noreply.github.com>
2023-12-21 19:36:30 -0300

    condições

---
+++ curso-em-video/condições/ex001.js
@@ -0,0 +1,7 @@
+const max_velocidade = 60
+var velocidade = 70
+console.log(`A velocidade do seu carro é ${velocidade}`)
+if (velocidade > max_velocidade) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado! Velocidade maxima: ${max_velocidade}`)
+}
+console.log('Dirija sempre usando cinto de segurança')
